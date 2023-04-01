3aa0ca40ca66240217a5c66bb7a74b4cc98adca4
Aditta Chakraborty <adittacse@gmail.com>
2023-04-01 23:55:28 +0600

    title updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,10 +6,17 @@ import Header from "./components/Header/Header.jsx";
 import Blog from "./components/Blog/Blog.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+    function Title() {
+        useEffect(() => {
+            document.title = 'Knowledge Cafe';
+        }, []);
+    }
 
   return (
     <div className="App">
+        <Title></Title>
         <Header></Header>
         <Blog></Blog>
     </div>
